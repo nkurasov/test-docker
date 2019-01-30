89583e4c6dce3c94bcbcd5d1a90ac28819cf111c
Nikita Kurasov <nkurasov@crimtan.com>
2019-01-30 16:17:19 +0300

    use configuration properties for surname service

--- hello-app/src/main/java/ru/test/hello/App.java
+++ hello-app/src/main/java/ru/test/hello/App.java
@@ -2,6 +2,7 @@ package ru.test.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,5 +19,11 @@ public class App {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    @ConfigurationProperties("ru.test.surname-service")
+    public SurnameService surnameService() {
+        return new SurnameService(restTemplate());
     }
 }
--- hello-app/src/main/java/ru/test/hello/SurnameService.java
+++ hello-app/src/main/java/ru/test/hello/SurnameService.java
@@ -1,7 +1,5 @@
 package ru.test.hello;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -11,7 +9,6 @@ import java.util.Objects;
 /**
  * @author Nikita Kurasov
  */
-@Service
 public class SurnameService {
 
     private final RestTemplate template;
@@ -39,17 +36,14 @@ public class SurnameService {
         return template.getForObject(uri, String.class);
     }
 
-    @Value("${ru.test.surname-service.protocol}")
     public void setProtocol(String protocol) {
         this.protocol = protocol;
     }
 
-    @Value("${ru.test.surname-service.host}")
     public void setHost(String host) {
         this.host = host;
     }
 
-    @Value("${ru.test.surname-service.port}")
     public void setPort(int port) {
         this.port = port;
     }
